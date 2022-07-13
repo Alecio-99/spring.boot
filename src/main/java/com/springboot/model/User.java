a2cda7aa83527953cb2a6d72d7e7983a49b4a344
Alacio Araujo <aleciosilveira97@gmail.com>
2022-07-13 15:44:32 -0300

    Spring Boot

---
+++ src/main/java/com/springboot/model/User.java
@@ -0,0 +1,32 @@
+package com.springboot.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String username;
+
+}
